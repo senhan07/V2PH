6f9ee2d85484d04f58eb7d42fd528f1382797da5
SenHan <34976706+senhan07@users.noreply.github.com>
2024-03-23 19:27:56 +0700

    test custom UA

--- module/driver.py
+++ module/driver.py
@@ -6,6 +6,9 @@ def run_engine():
     options = uc.ChromeOptions()
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    # test_ua = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
+    # options.add_argument(f'--user-agent={test_ua}')
+    # options.add_argument("--headless")
     driver = uc.Chrome(options=options)
     driver.set_window_size(800, 600) 
     return driver
