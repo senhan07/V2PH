0123006ab1ed4d17fd3368c845feb330d9cb18bb
SenHan <34976706+senhan07@users.noreply.github.com>
2024-09-13 13:19:10 +0700

    switch UC to selenium

--- module/driver.py
+++ module/driver.py
@@ -1,14 +1,12 @@
-import undetected_chromedriver as uc
+from seleniumbase import Driver
 
-#! Initialize start the chrome engine
 def run_engine():
     print("\nStarting chrome engine...")
-    options = uc.ChromeOptions()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    # test_ua = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
-    # options.add_argument(f'--user-agent={test_ua}')
-    # options.add_argument("--headless")
-    driver = uc.Chrome(options=options)
-    driver.set_window_size(800, 600) 
+
+    # Initialize the driver instance
+    driver = Driver(uc=True, headless=False,)
     return driver
+
+def solve_turnstile(driver):
+    driver.driver.uc_switch_to_frame("iframe")
+    driver.driver.uc_click("span.mark")
